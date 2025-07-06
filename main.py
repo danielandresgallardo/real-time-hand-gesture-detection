1ffcd9c535763c9fd7a2e47d57881584778caaf7
Daniel Gallardo <80461972+danielandresgallardo@users.noreply.github.com>
2025-07-06 09:52:21 +0800

    Main python code thet runs both processes.

---
+++ main.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process, Queue
+from gesture_detector import run_gesture_detection
+from hud_display import run_hud
+from multiprocessing import Process, Queue, Event
+from gesture_detector import run_gesture_detection
+from hud_display import run_hud
+
+if __name__ == "__main__":
+    queue = Queue()
+    stop_event = Event()
+
+    p1 = Process(target=run_gesture_detection, args=(queue, stop_event))
+    p2 = Process(target=run_hud, args=(queue, stop_event))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
